fa2b34d88ad81f1f60f92d8c6d88cde4e82ccf46
KonstantIMP <mihedovkos@gmail.com>
2021-02-17 00:15:10 +0300

    Database is OK

---
+++ server/database.py
@@ -0,0 +1,26 @@
+from flask import jsonify
+from pony import orm
+
+class Database :
+    db = orm.Database()
+
+    class Person(db.Entity) :
+        name = orm.Required(str)
+        surname = orm.Required(str)
+        patronymic = orm.Required(str)
+
+        login = orm.Required(str)
+        passwd = orm.Required(str)
+
+    def __init__(self, host_address, username, passwd, db_name):
+        self.db.bind(provider='mysql', user=username, password=passwd, host=host_address, database=db_name)
+        self.db.generate_mapping(create_tables=True)
+        orm.set_sql_debug(True)
+
+    @orm.db_session
+    def get_user(self, user_surname) :
+        user = self.Person.get(surname='Михедов')
+
+        if user : return jsonify({'status' : 'ok', 'name' : user.name, 'surname' : user.surname, 'patronymic' : user.patronymic, 'login' : user.login, 'passwd' : user.passwd})
+        else : return jsonify({'status' : 'error'})
+
